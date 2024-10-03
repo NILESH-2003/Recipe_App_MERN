ccbbeaa49db168fa43302b4f8efd3a0d43de5e6d
Nilesh <nira21cs@cmrit.ac.in>
2024-10-04 02:14:13 +0530

    first commit

---
+++ API/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+
+mongoose
+  .connect(
+    "mongodb+srv://nira21cs:qdwXA1FKqcrwAWc5@cluster0.mgvxr.mongodb.net/",
+  
+    {
+      dbName: "Recipe App",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
+// username = nira21cs;
+// password = qdwXA1FKqcrwAWc5
